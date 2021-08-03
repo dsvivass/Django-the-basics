68befaebe85d4d669205494a737a98c1a9556d18
dsvivass <dsvivass@unal.edu.co>
2021-08-03 15:18:35 -0500

    Se anade base de datos y superusuario de django

---
+++ PROYECTO/project1/aplicaciones/principal/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+# aqui es donde vamos a escribir la interpretacion de las tablas de nuestra base de datos
+
+class Persona(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=120)
+    correo = models.EmailField(max_length=200)
