290fe8b7c5dbe90e96ce143d832201e4a2baff82
dsvivass <dsvivass@unal.edu.co>
2021-08-03 15:59:42 -0500

    Se arregla el fallo de seguridad

--- PROYECTO/project1/aplicaciones/principal/views.py
+++ PROYECTO/project1/aplicaciones/principal/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Persona
+
+def inicio(request):
+    return render(request, 'index.html') # El template es index.html
